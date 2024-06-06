a22e4d44dcd4ff4ef6b530f729be9e970f7df376
Rishabh Agarwal <rishabh052003@gmail.com>
2024-06-06 11:25:02 +0530

    Add files via upload

---
+++ array1/src/array1/four.java
@@ -0,0 +1,16 @@
+package array1;
+
+public class four {
+	static int[] get()
+	{  
+		return new int[]{10,30,50,60,80};  
+		}  
+		  
+		public static void main(String args[]){  
+		//calling method which returns an array  
+		int arr[]=get();  
+		//printing the values of an array  
+		for(int i=0;i<arr.length;i++)  
+		System.out.println(arr[i]);  
+		}}  
+

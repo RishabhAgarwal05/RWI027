fc4c28b91472ceaaa04c90481ac5715dffb922d7
Rishabh Agarwal <rishabh052003@gmail.com>
2024-06-06 11:20:45 +0530

    Add files via upload

---
+++ arrayshort/src/arrayshort/five.java
@@ -0,0 +1,20 @@
+package arrayshort;
+
+import java.util.Arrays;
+
+public class five 
+{
+	 public static void main(String[] args)   
+	{   
+	  
+	int [] array = new int [] {77, 23, 5, 150, 12, 99, 55, 34};  
+	  
+	System.out.println("Elements of array sorted in ascending order: ");  
+	Arrays.sort(array);
+	for (int i = 0; i < array.length; i++)   
+	{       
+	System.out.println(array[i]);   
+	}   
+	}  
+	}  
+

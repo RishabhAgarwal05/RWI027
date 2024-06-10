19b216df37d79a0235e281f77a01c14e675a3163
Rishabh Agarwal <rishabh052003@gmail.com>
2024-06-10 14:35:21 +0530

    Add files via upload

---
+++ exceptionhandling/src/exceptionhandling/my.java
@@ -0,0 +1,18 @@
+package exceptionhandling;
+
+public class my {
+	public static void main(String[] args) {
+		System.out.println("30000");
+		 try  
+	        {
+	        }  
+	            //handling the exception  
+	        catch(Exception e)  
+	        {  
+	            System.out.println(e);  
+	        }  
+	        System.out.println("Railworld");  
+	    }  
+	      
+	}  
+	
